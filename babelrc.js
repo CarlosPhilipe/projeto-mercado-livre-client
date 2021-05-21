a86738c3c31023ad30b0acdc7594aa4a03b0b286
CarlosPhilipe <carlos.bahia@gmail.com>
2021-05-20 21:26:53 -0400

    build: Configura styled-components no projeto

---
+++ babelrc.js
@@ -0,0 +1,15 @@
+export default {
+  "plugins": [
+    [
+      "babel-plugin-styled-components",
+      {
+        "ssr": true,
+      }
+    ]
+  ],
+  presets: [
+    [
+      "next/babel"
+    ],
+  ],
+}
