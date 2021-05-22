914a1b6907cfe03fb69085312812ed0662dd4db8
CarlosPhilipe <carlos.bahia@gmail.com>
2021-05-22 13:16:59 -0400

    feat: Adiciona ProductItem a página /items

--- src/pages/items/Items.jsx
+++ src/pages/items/Items.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { shape, arrayOf, string } from 'prop-types';
 
+import getInitialProps from '../../server/Items';
 import MainTemplate from '../../template/MainTemplate';
+import ProductItem from '../../components/ProductItem';
+function Items(props) {
+  const { items } = props;
 
-function Items() {
   return (
-    <MainTemplate>Items</MainTemplate>
+    <MainTemplate>
+      {items.map((item) => {
+        return (<ProductItem key={item.id} item={item} />)
+      })}
+    </MainTemplate>
   );
 }
+
+Items.propTypes = {
+  items: arrayOf(shape({})),
+  categories: arrayOf(string),
+};
+
+Items.defaulfProps = {
+  items: [],
+  categories: [],
+};
+
+Items.getInitialProps = getInitialProps;
 
 export default Items;
