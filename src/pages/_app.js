6d274e55e413bd1b0ea5b45acb963071f442f1c0
CarlosPhilipe <carlos.bahia@gmail.com>
2021-05-21 23:45:36 -0400

    feat: Adiciona title e meta description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shape, func } from 'prop-types';
 import GlobalStyle from '../styles/global.style';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Mercado livre</title>
+        <meta name="description" content="Teste prático de frontend" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
