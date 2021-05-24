c1262fefb59c41abea18985dcfb8e21db4ea417b
CarlosPhilipe <carlos.bahia@gmail.com>
2021-05-23 20:27:33 -0400

    feat: Adiciona role em BreadCrumb

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -6,19 +6,17 @@ import Style from './BreadCrumb.styles';
 function BreadCrumb ({ categories }) {
   return (
     <Style.Container id='bread_crumb'>
-      <Style.UnumeradList>
-        {categories.map((category, index) => {
-          return (
-            <Style.ListItem
-              key={category}
-              lastItem={(index === categories.length - 1)}
-            >
-              {category} {(index !== categories.length - 1) && (
-                <span>{' >'}</span>
-              )}
-            </Style.ListItem>
-          );
-        })}
+      <Style.UnumeradList role='bread_crumb'>
+        {categories.map((category, index) => (
+          <Style.ListItem
+            key={category}
+            lastItem={(index === categories.length - 1)}
+          >
+            {category} {(index !== categories.length - 1) && (
+              <span>{' >'}</span>
+            )}
+          </Style.ListItem>
+        ))}
       </Style.UnumeradList>
     </Style.Container>
   );
